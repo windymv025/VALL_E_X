81fe218ec64c46b7ba71aab4e001c4d2555757ac
ELCANET\phav <vuong.phamminh@elca.vn>
2024-10-13 02:17:11 +0700

    fix bug make dataset

--- macros.py
+++ macros.py
@@ -9,6 +9,7 @@ lang2token = {
     'zh': "[ZH]",
     'ja': "[JA]",
     "en": "[EN]",
+    "vi": "[VI]",
     'mix': "",
 }
 
@@ -16,12 +17,14 @@ lang2code = {
     'zh': 0,
     'ja': 1,
     "en": 2,
+    "vi": 3
 }
 
 token2lang = {
     '[ZH]': "zh",
     '[JA]': "ja",
     "[EN]": "en",
+    "[VI]": "vi",
     "": "mix"
 }
 
@@ -29,11 +32,13 @@ code2lang = {
     0: 'zh',
     1: 'ja',
     2: "en",
+    3: "vi",
 }
 
 langdropdown2token = {
     'English': "[EN]",
     '中文': "[ZH]",
     '日本語': "[JA]",
+    'Tiếng Việt': "VI",
     'Mix': "",
 }
